26acc5db8db1aa53dfac9c28e36266cbbe41f079
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-11-11 16:48:57 +0100

    build: adding example with text input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bevy::prelude::*;
+use bevy_simple_text_input::{TextInput, TextInputPlugin};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(TextInputPlugin)
+        .add_systems(Startup, setup)
+        .run();
 }
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        TextInput,
+        Node {
+            padding: UiRect::all(Val::Px(5.0)),
+            border: UiRect::all(Val::Px(2.0)),
+            ..default()
+        },
+        BorderColor::all(Color::BLACK)
+    ));
+}
+
+// fn main() {
+//     println!("Hello, world!");
+// }
